49e88f17efdc2ce4a6ed25301098424660075df5
Eric Furspan <eric.furspan@gmail.com>
2018-03-22 19:40:50 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const blogPostsRouter = require('./BlogPostsRouter');
+
+// log the http layer
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.use('/blog-posts', blogPostsRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
