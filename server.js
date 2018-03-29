8d01b1bdcf0a14d8a4fc19defe80326a6f3dea6a
Eric Furspan <eric.furspan@gmail.com>
2018-03-29 17:02:24 -0400

    deleted BlogPostsRouter, added config.js, created GET routes with mongoose

--- server.js
+++ server.js
@@ -1,50 +1,101 @@
-
+const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+const { BlogPost } = require('./models');
+
 
 const app = express();
 
-const blogPostsRouter = require('./BlogPostsRouter');
-
-// log the http layer
+app.use(bodyParser.json());
 app.use(morgan('common'));
 
-// import static assets
-app.use(express.static('public'));
 
-// import blog-posts router
-app.use('/blog-posts', blogPostsRouter);
+// GET requests to /posts => return all blog posts
+app.get('/posts', (req, res) => {
+    BlogPost
+      .find()
+      .then(posts => {
+        res.json({
+          blogposts: posts.map(
+            (post) => post.serialize())
+        });
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+      });
+});
+
+// GET requests to /posts/:id => return a single blog post
+app.get('/posts/:id', (req, res) => {
+    BlogPost
+      .findById(req.params.id)
+      .then(post => res.json(post.serialize()))
+      .catch(err => {
+          console.error(err);
+          res.status(500).json({ error: 'Internal server error' });
+      });
+})
+
+/*
+// POST requests to /posts => creates and returns a new post 
+app.post('/posts', (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    requiredFields.forEach((field) => {
+        if()
+    })
+})
+*/
+
+
 
 let server;
 
-function runServer() {
-    const port = process.env.PORT || 8080;
-    return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            console.log(`App listening on port ${port}`);
-            resolve(server);
-        }).on('error', err => {
-            reject(err);
+// this function connects to our database, then starts the server
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
         });
     });
+  });
 }
 
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
 function closeServer() {
+  return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-        console.log('Closing server');
-        server.close(err => {
-            if (err) {
-                reject(err);
-                return;
-            } 
-            resolve();
-        });
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
+  });
 }
 
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
 
-
-module.exports = {app, runServer, closeServer};
+module.exports = { app, runServer, closeServer };
